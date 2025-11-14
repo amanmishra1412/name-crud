6890f93377dee08ac7aa36e99b1ac3aa7026dda8
amanmishra1412 <mishra06aman@gmail.com>
2025-11-14 11:33:17 +0530

    cors change

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,11 @@ const ENV_CONFIG = require("./config/env");
 const port = ENV_CONFIG.PORT || 10000;
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: "*", // testing ke liye, later specific frontend URL bhi use kar sakte ho
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
